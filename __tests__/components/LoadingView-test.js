662531c3479f442c25ddde209cf895dd8add1d42
ryuichi.ogawa <ryuichi.ogawa@geofusion.com.br>
2018-05-30 10:36:49 -0300

    Melhorando caso de test

--- __tests__/components/LoadingView-test.js
+++ __tests__/components/LoadingView-test.js
@@ -1,15 +1,37 @@
 import React from 'react'
 import LoadingView from '../../src/components/LoadingView'
 import renderer from 'react-test-renderer'
+import { Text } from 'react-native'
 
-test('test make sure renders correctly', () => {
-  const component = (
-    <LoadingView loading />
-  )
+describe('LoadingView Component Test', () => {
+  test('test make sure renders correctly', () => {
+    let component = (
+      <LoadingView loading />
+    )
+    let tree = renderer.create(component)
+    expect(tree.toJSON()).toMatchSnapshot()
+  })
 
-  const tree = renderer.create(component)
-  const testInstance = tree.root
+  test('Should Render Loading when Props loading is True Test', () => {
+    let component = (
+      <LoadingView loading />
+    )
+    let tree = renderer.create(component)
+    let testInstance = tree.root
+    expect(testInstance.findByType(LoadingView).props.loading).toBeTruthy()
+  })
 
-  expect(tree.toJSON()).toMatchSnapshot()
-  expect(testInstance.findByType(LoadingView).props.loading).toBeTruthy()
+  test('Should Render Component when Props loading is False Test', () => {
+    let Component = (
+      <LoadingView loading={false}>
+        <Text>Hello</Text>
+      </LoadingView>
+    )
+    let componentExpect = <Text accessible allowFontScaling ellipsizeMode='tail'>Hello</Text>
+    let tree = renderer.create(Component)
+    let testInstance = tree.root
+
+    expect(testInstance.findByType(LoadingView).props.loading).toBeFalsy()
+    expect(testInstance.findByType(LoadingView).props.children).toEqual(componentExpect)
+  })
 })
