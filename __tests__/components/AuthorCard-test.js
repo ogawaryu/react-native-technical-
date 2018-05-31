ff8bd1548f0734de0380133d62fbb7ebe37c7f41
Ryuichi Ogawa <ogawaryuichi@gmail.com>
2018-05-31 11:51:32 -0300

    Melhorando cobertura de tests

--- __tests__/components/AuthorCard-test.js
+++ __tests__/components/AuthorCard-test.js
@@ -2,18 +2,38 @@ import React from 'react'
 import AuthorCard from '../../src/components/AuthorCard'
 import renderer from 'react-test-renderer'
 
-test('test make sure renders correctly', () => {
-  let userName = 'UserName'
-  let urlPath = 'http://i.pravatar.cc/300'
-  const component = (
-    <AuthorCard
-      userName={userName}
-      imagePath={urlPath} />
-  )
-  const tree = renderer.create(component)
-  const testInstance = tree.root
+let userName = 'UserName'
+let urlPath = 'http://i.pravatar.cc/300'
 
-  expect(tree.toJSON()).toMatchSnapshot()
-  expect(testInstance.findByType(AuthorCard).props.userName).toBe(userName)
-  expect(testInstance.findByType(AuthorCard).props.imagePath).toBe(urlPath)
+const component = (
+  <AuthorCard
+    userName={userName}
+    imagePath={urlPath}
+    alignLeft />
+)
+
+const tree = renderer.create(component)
+const testInstance = tree.root
+
+describe('AuthorCard Component Test', () => {
+
+  test('test make sure renders correctly', () => {
+    expect(tree.toJSON()).toMatchSnapshot()
+  })
+
+  test('Should Render AuthorCard when Passing Props userName and imagePath Test', () => {
+    expect(testInstance.findByType(AuthorCard).props.userName).toBe(userName)
+    expect(testInstance.findByType(AuthorCard).props.imagePath).toBe(urlPath)
+    expect(testInstance.findByType(AuthorCard).props.alignLeft).toBeTruthy()
+  })
+
+  test('Should Render AuthorCard Default Props Value when No Passing Props Test', () => {
+    let testInstanceLocal = renderer.create((
+      <AuthorCard />
+    )).root
+
+    expect(testInstanceLocal.findByType(AuthorCard).props.userName).toBe('')
+    expect(testInstanceLocal.findByType(AuthorCard).props.imagePath).toBe('')
+    expect(testInstanceLocal.findByType(AuthorCard).props.alignLeft).toBeFalsy()
+  })
 })
--- __tests__/components/LoadingView-test.js
+++ __tests__/components/LoadingView-test.js
@@ -4,6 +4,7 @@ import renderer from 'react-test-renderer'
 import { Text } from 'react-native'
 
 describe('LoadingView Component Test', () => {
+
   test('test make sure renders correctly', () => {
     let component = (
       <LoadingView loading />
@@ -22,16 +23,14 @@ describe('LoadingView Component Test', () => {
   })
 
   test('Should Render Component when Props loading is False Test', () => {
-    let Component = (
+    let testInstanceLocal = renderer.create((
       <LoadingView loading={false}>
         <Text>Hello</Text>
       </LoadingView>
-    )
+    )).root
     let componentExpect = <Text accessible allowFontScaling ellipsizeMode='tail'>Hello</Text>
-    let tree = renderer.create(Component)
-    let testInstance = tree.root
 
-    expect(testInstance.findByType(LoadingView).props.loading).toBeFalsy()
-    expect(testInstance.findByType(LoadingView).props.children).toEqual(componentExpect)
+    expect(testInstanceLocal.findByType(LoadingView).props.loading).toBeFalsy()
+    expect(testInstanceLocal.findByType(LoadingView).props.children).toEqual(componentExpect)
   })
 })
