0921627c83b26176892d91223392b79123d2f320
Ryuichi Ogawa <ogawaryuichi@gmail.com>
2018-05-31 12:23:55 -0300

    Melhorando cobertura de tests

--- __tests__/components/ScoreView-test.js
+++ __tests__/components/ScoreView-test.js
@@ -2,20 +2,33 @@ import React from 'react'
 import ScoreView from '../../src/components/ScoreView'
 import renderer from 'react-test-renderer'
 
-test('test make sure renders correctly', () => {
-  let forkAmount = 10
-  let starAmount = 100
+const forkAmount = 10
+const starAmount = 100
 
-  const component = (
-    <ScoreView
-      forkAmount={forkAmount}
-      starAmount={starAmount} />
-  )
+const component = (
+  <ScoreView
+    forkAmount={forkAmount}
+    starAmount={starAmount} />
+)
+const tree = renderer.create(component)
+const testInstance = tree.root
 
-  const tree = renderer.create(component)
-  const testInstance = tree.root
+describe('ScoreView Component Test', () => {
 
-  expect(tree.toJSON()).toMatchSnapshot()
-  expect(testInstance.findByType(ScoreView).props.forkAmount).toBe(forkAmount)
-  expect(testInstance.findByType(ScoreView).props.starAmount).toBe(starAmount)
+  test('test make sure renders correctly', () => {
+    expect(tree.toJSON()).toMatchSnapshot()
+  })
+
+  test('Should Render ScoreView when Passing Props forkAmount and starAmount Test', () => {
+    expect(testInstance.findByType(ScoreView).props.forkAmount).toBe(forkAmount)
+    expect(testInstance.findByType(ScoreView).props.starAmount).toBe(starAmount)
+  })
+
+  test('Should Render ScoreView Default Props when Empty Props Test', () => {
+    let testInstanceLocal = renderer.create((
+      <ScoreView />
+    )).root
+    expect(testInstanceLocal.findByType(ScoreView).props.forkAmount).toBe(0)
+    expect(testInstanceLocal.findByType(ScoreView).props.starAmount).toBe(0)
+  })
 })
--- __tests__/components/SubTitle-test.js
+++ __tests__/components/SubTitle-test.js
@@ -2,20 +2,33 @@ import React from 'react'
 import SubTitle from '../../src/components/SubTitle'
 import renderer from 'react-test-renderer'
 
-test('test make sure renders correctly', () => {
-  let opened = 10
-  let closed = 100
+const opened = 10
+const closed = 100
 
-  const component = (
-    <SubTitle
-      opened={opened}
-      closed={closed} />
-  )
+const component = (
+  <SubTitle
+    opened={opened}
+    closed={closed} />
+)
+const tree = renderer.create(component)
+const testInstance = tree.root
 
-  const tree = renderer.create(component)
-  const testInstance = tree.root
+describe('SubTitle Component Test', () => {
 
-  expect(tree.toJSON()).toMatchSnapshot()
-  expect(testInstance.findByType(SubTitle).props.opened).toBe(opened)
-  expect(testInstance.findByType(SubTitle).props.closed).toBe(closed)
+  test('test make sure renders correctly', () => {
+    expect(tree.toJSON()).toMatchSnapshot()
+  })
+
+  test('Should Render SubTitle when Passing Props opened and closed Test', () => {
+    expect(testInstance.findByType(SubTitle).props.opened).toBe(opened)
+    expect(testInstance.findByType(SubTitle).props.closed).toBe(closed)
+  })
+
+  test('Should Render SubTitle Default Props when Empty Props Test', () => {
+    let testInstanceLocal = renderer.create((
+      <SubTitle />
+    )).root
+    expect(testInstanceLocal.findByType(SubTitle).props.opened).toBe(0)
+    expect(testInstanceLocal.findByType(SubTitle).props.closed).toBe(0)
+  })
 })
